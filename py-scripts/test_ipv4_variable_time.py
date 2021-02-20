b7ad13ab3622ce489c0f887719225c6299e28da3
Dipti <dipti.dhond@candelatech.com>
2021-02-19 18:36:58 -0800

    changed placement of logic to check monitor args
    Signed-off-by: Dipti <dipti.dhond@candelatech.com>

--- py-scripts/test_ipv4_variable_time.py
+++ py-scripts/test_ipv4_variable_time.py
@@ -278,50 +278,6 @@ python3 ./test_ipv4_variable_time.py
     if (args.num_stations is not None) and (int(args.num_stations) > 0):
         num_sta = int(args.num_stations)
 
-    # Create directory
-
-    # if file path with output file extension is not given...
-    # check if home/lanforge/report-data exists. if not, save 
-    # in new folder based in current file's directory
-
-    if args.report_file is None:
-        new_file_path = str(datetime.datetime.now().strftime("%Y-%m-%d-%H-h-%M-m-%S-s")).replace(':',
-                                                                                        '-') + '-test_ipv4_variable_time'  # create path name
-        try:
-            path = os.path.join('/home/lanforge/report-data/', new_file_path)
-            os.mkdir(path)
-        except:
-            curr_dir_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-            path = os.path.join(curr_dir_path, new_file_path)
-            os.mkdir(path)
-
-        if args.output_format in ['csv', 'json', 'html', 'hdf','stata', 'pickle', 'pdf', 'png', 'parquet',
-                                  'xlsx']:
-            report_f = str(path) + '/data.' + args.output_format
-            output = args.output_format
-        else:
-            print('Not supporting this report format or cannot find report format provided. Defaulting to csv data file output type, naming it data.csv.')
-            report_f = str(path) + '/data.csv'
-            output = 'csv'
-            
-    else:
-        report_f = args.report_file
-        if args.output_format is None:
-            output = str(args.report_file).split('.')[-1]
-        else:
-            output = args.output_format
-    print("Saving final report data in ... " + report_f)
-
-    # Retrieve last data file
-    compared_rept = None
-    if args.compared_report:
-        compared_report_format=args.compared_report.split('.')[-1]
-        #if compared_report_format not in ['csv', 'json', 'dta', 'pkl','html','xlsx','parquet','h5']:
-        if compared_report_format != 'csv':
-            print(ValueError("Cannot process this file type. Please select a different file and re-run script."))
-            exit(1)
-        else:
-            compared_rept=args.compared_report
             
     station_list = LFUtils.portNameSeries(prefix_="sta", start_id_=0, end_id_=num_sta - 1, padding_number_=10000,
                                           radio=args.radio)
@@ -350,31 +306,76 @@ python3 ./test_ipv4_variable_time.py
         ip_var_test.exit_fail()
     ip_var_test.start(False, False)
 
-    try:
-        layer3connections = ','.join([[*x.keys()][0] for x in ip_var_test.json_get('endp')['endpoint']])
-    except:
-        raise ValueError('Try setting the upstream port flag if your device does not have an eth1 port')
-
-    if type(args.layer3_cols) is not list:
-        layer3_cols = list(args.layer3_cols.split(","))
-        # send col names here to file to reformat
-    else:
-        layer3_cols = args.layer3_cols
-        # send col names here to file to reformat
-    if type(args.port_mgr_cols) is not list:
-        port_mgr_cols = list(args.port_mgr_cols.split(","))
-        # send col names here to file to reformat
-    else:
-        port_mgr_cols = args.port_mgr_cols
-        # send col names here to file to reformat
-    if args.debug:
-        print("Layer 3 Endp column names are...")
-        print(layer3_cols)
-        print("Port Manager column names are...")
-        print(port_mgr_cols)
-
     monitor_interval = Realm.parse_time(args.monitor_interval).total_seconds()
     if args.monitor:
+    # Create directory
+
+    # if file path with output file extension is not given...
+    # check if home/lanforge/report-data exists. if not, save 
+    # in new folder based in current file's directory
+
+        if args.report_file is None:
+            new_file_path = str(datetime.datetime.now().strftime("%Y-%m-%d-%H-h-%M-m-%S-s")).replace(':',
+                                                                                            '-') + '-test_ipv4_variable_time'  # create path name
+            try:
+                path = os.path.join('/home/lanforge/report-data/', new_file_path)
+                os.mkdir(path)
+            except:
+                curr_dir_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+                path = os.path.join(curr_dir_path, new_file_path)
+                os.mkdir(path)
+
+            if args.output_format in ['csv', 'json', 'html', 'hdf','stata', 'pickle', 'pdf', 'png', 'parquet',
+                                    'xlsx']:
+                report_f = str(path) + '/data.' + args.output_format
+                output = args.output_format
+            else:
+                print('Not supporting this report format or cannot find report format provided. Defaulting to csv data file output type, naming it data.csv.')
+                report_f = str(path) + '/data.csv'
+                output = 'csv'
+                
+        else:
+            report_f = args.report_file
+            if args.output_format is None:
+                output = str(args.report_file).split('.')[-1]
+            else:
+                output = args.output_format
+        print("Saving final report data in ... " + report_f)
+        
+        compared_rept=None
+        if args.compared_report:
+            compared_report_format=args.compared_report.split('.')[-1]
+            #if compared_report_format not in ['csv', 'json', 'dta', 'pkl','html','xlsx','parquet','h5']:
+            if compared_report_format != 'csv':
+                print(ValueError("Cannot process this file type. Please select a different file and re-run script."))
+                exit(1)
+            else:
+                compared_rept=args.compared_report
+
+        try:
+            layer3connections = ','.join([[*x.keys()][0] for x in ip_var_test.json_get('endp')['endpoint']])
+        except:
+            raise ValueError('Try setting the upstream port flag if your device does not have an eth1 port')
+
+        if type(args.layer3_cols) is not list:
+            layer3_cols = list(args.layer3_cols.split(","))
+            # send col names here to file to reformat
+        else:
+            layer3_cols = args.layer3_cols
+            # send col names here to file to reformat
+        if type(args.port_mgr_cols) is not list:
+            port_mgr_cols = list(args.port_mgr_cols.split(","))
+            # send col names here to file to reformat
+        else:
+            port_mgr_cols = args.port_mgr_cols
+            # send col names here to file to reformat
+        if args.debug:
+            print("Layer 3 Endp column names are...")
+            print(layer3_cols)
+            print("Port Manager column names are...")
+            print(port_mgr_cols)
+
+        #start monitor 
         ip_var_test.l3cxprofile.monitor(layer3_cols=layer3_cols,
                                     sta_list=station_list,
                                     port_mgr_cols=port_mgr_cols,
@@ -387,7 +388,6 @@ python3 ./test_ipv4_variable_time.py
                                     script_name='test_ipv4_variable_time',
                                     arguments=args,
                                     debug=args.debug)
-
     ip_var_test.stop()
     if not ip_var_test.passes():
         print(ip_var_test.get_fail_message())
