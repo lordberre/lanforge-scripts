f6d127423eb67865eb9f05de6a1cc85f575f6ea5
Björn Lennestål <jakerlock@gmail.com>
2023-01-31 10:21:48 +0100

    wip

--- py-scripts/tele2_rvr.py
+++ py-scripts/tele2_rvr.py
@@ -24,6 +24,8 @@ from LANforge.lfcli_base import LFCliBase
 from LANforge.LFUtils import *
 import realm
 from realm import Realm
+from datetime import datetime
+import json
 
 if sys.version_info[0] != 3:
     logger.critical("This script requires Python 3")
@@ -36,7 +38,7 @@ if 'py-json' not in sys.path:
 # lf_logger_config = importlib.import_module("py-scripts.lf_logger_config")
 # logger = logging.getLogger(__name__)
 
-class CreateAttenuator(Realm):
+class CreateRvRAttenuator(Realm):
     def __init__(self, host, port, serno, idx, val,
                  _debug_on=False,
                  _exit_on_error=False,
@@ -55,6 +57,7 @@ class CreateAttenuator(Realm):
         self.defaults.atten_serno = 'all'
         self.defaults.atten_idx = 'all'
         self.defaults.atten_val = 955
+        self.ddb_step_size = 50  # Example: 50 = 0.5 dB attenuation added for each step in the RvR test
 
     def _build(self):
         self.attenuator_profile.create()
@@ -83,38 +86,26 @@ class CreateAttenuator(Realm):
     def base_profile(self, iteration=0):
         self.attenuator_profile = self.new_attenuator_profile()
         self._configure('3066', 'all', 955)
-        self._configure('3067', 'all', 955)        # Throttlers chamber (Node2) (Air)
+        self._configure('3067', 'all', 955)        # Node2 (Air)
         self._configure('3068', 'all', 955)
-        self._configure('3070', 'all', 0)          # Client running service (Node1)
-        self._configure('3073', 0, 500)            # Conducted Node2 - Needs to be open for throttlers
-        self._configure('3073', 1, 500)            # Conducted Node2 - Needs to be open for throttlers
-        self._configure('3073', 2, 955)            # Conducted Node1 - Needs to be completely attenuated
-        self._configure('3073', 3, 955)            # Conducted Node1 - Needs to be completely attenuated
+        self._configure('3070', 'all', 955)        # Node1
+        self._configure('3073', 0, 955)            # Conducted Node2
+        self._configure('3073', 1, 955)            # Conducted Node2
+        self._configure('3073', 2, 955)            # Conducted Node1
+        self._configure('3073', 3, 955)            # Conducted Node1
         self._configure('3076', 'all', 955)
-        self._configure('3084', 'all', 955)
+        self._configure('3084', 'all', 955)        # RvR client in MobileStations chamber
         self._apply()
         self._print(iteration, 0)
 
-    def disabled_attenuation_profile(self, iteration):
-        self._configure('3070', 'all', 0)
+    def increase_attenuation(self, iteration):
+        self._configure('3084', 'all', self.ddb_step_size)
         self._apply()
         self._print(iteration, 0)
-    def low_attenuation_profile(self, iteration):
-        self._configure('3070', 'all', 100)
-        self._apply()
-        self._print(iteration, 10)
-    def mid_attenuation_profile(self, iteration):
-        self._configure('3070', 'all', 200)
-        self._apply()
-        self._print(iteration, 20)
-    def high_attenuation_profile(self, iteration):
-        self._configure('3070', 'all', 300)
-        self._apply()
-        self._print(iteration, 30)
 
 
 class Tele2RateVersusRange(LFCliBase):
-    def __init__(self, lfclient_host, lfclient_port, ssid, paswd, security, radio, sta_list=None, name_prefix="T2RvR", upstream="eth1", traffic_direction='downstream'):
+    def __init__(self, lfclient_host, lfclient_port, ssid, paswd, security, radio, duts, sta_list=None, name_prefix="T2RvR", upstream="eth1", traffic_direction='downstream'):
         super().__init__(lfclient_host, lfclient_port)
         self.host = lfclient_host
         self.port = lfclient_port
@@ -126,6 +117,7 @@ class Tele2RateVersusRange(LFCliBase):
         self.name_prefix = name_prefix
         self.upstream = upstream
         self.traffic_direction = traffic_direction
+        self.duts = duts
 
         self.local_realm = realm.Realm(lfclient_host=self.host, lfclient_port=self.port)
         self.station_profile = self.local_realm.new_station_profile()
@@ -136,18 +128,51 @@ class Tele2RateVersusRange(LFCliBase):
         self.cx_profile.host = self.host
         self.cx_profile.port = self.port
         self.cx_profile.name_prefix = self.name_prefix
+        self.attenuator = CreateRvRAttenuator(host=self.host, port=self.port, serno='all', idx='all', val=955)
 
+        # RvR settings
+        self.num_iterations = 1  # Number of full iterations (0 -> 95 dB) to perform before quitting.
+        self.step_length_sec = 15  # Number of seconds to run traffic per attenuation step
+
+        requested_tput = 10000000000  # 10 Gbit/s
         if self.traffic_direction == 'downstream':
-            self.cx_profile.side_a_min_bps = 1e10
-            self.cx_profile.side_a_max_bps = 1e10
+            self.cx_profile.side_a_min_bps = requested_tput
+            self.cx_profile.side_a_max_bps = self.cx_profile.side_a_min_bps
             self.cx_profile.side_b_min_bps = 0
             self.cx_profile.side_b_max_bps = 0
         elif self.traffic_direction == 'upstream':
-            self.cx_profile.side_b_min_bps = 1e10
-            self.cx_profile.side_b_max_bps = 1e10
+            self.cx_profile.side_b_min_bps = requested_tput
+            self.cx_profile.side_b_max_bps = self.cx_profile.side_b_max_bps
             self.cx_profile.side_a_min_bps = 0
             self.cx_profile.side_a_max_bps = 0
 
+    def next_rvr_step(self):
+       pass
+
+    def wait_for_cx_to_start(self, cxs=None, timeout=300):
+        for x in cxs:
+            give_up_ts = time.time() + timeout
+            while True:
+                cx_state = self.json_get("/cx/{}".format(x), debug_=self.debug)
+                # print('CX {} state: {}'.format(x, cx_state))
+                if cx_state[x]['state'] == 'Run':
+                    print('Cross-endpoint {} has started: {}'.format(x, cx_state[x]['state']))
+                    break
+                else:
+                    if time.time() > give_up_ts:
+                        print('Error: Cross-endpoint never started after {} sec: {}. State: {}'.format(timeout, x, cx_state[x]['state']))
+                        break
+                    print('Waiting for Cross-endpoint {} to start.. State: {}'.format(x, cx_state[x]['state']))
+                    time.sleep(1)
+
+    # Create stations and l3 endpoints
+    def create_station(self, autostart=False):
+        self.station_profile.use_security(self.security, self.ssid, self.paswd)
+        self.station_profile.create(radio=self.radio, sta_names_=self.sta_list)
+        print('sta_names_: {}, station_profile.station_names: {}'.format(self.sta_list, self.station_profile.station_names))
+        self.cx_profile.create(endp_type="lf_tcp", side_a=self.station_profile.station_names, side_b=self.upstream, sleep_time=1)
+        if autostart:
+            self.start_station_traffic()
 
     def precleanup(self):
         self.cx_profile.cleanup_prefix()
@@ -171,9 +196,10 @@ class Tele2RateVersusRange(LFCliBase):
         time.sleep(5)
 
     def build(self):
-        self.load_blank_database()
+        # self.load_blank_database()
         resource = 1
-        # LFUtils.(self.host, self.sta_list)
+
+        # Initiate the upstream port
         try:
             data = LFUtils.port_dhcp_up_request(resource, self.upstream)
             print("set_port request data: {}".format(data))
@@ -182,14 +208,15 @@ class Tele2RateVersusRange(LFCliBase):
             print("LFUtils.port_dhcp_up_request didn't complete ")
             print("or the json_post failed either way {} did not set up dhcp so test may not pass data ".format(self.side_b))
 
-        print("password: {}".format(self.paswd))
-        self.station_profile.use_security(self.security, self.ssid, self.paswd)
-        self.station_profile.create(radio=self.radio, sta_names_=self.sta_list)
-        print('sta_names_: {}, station_profile.station_names: {}'.format(self.sta_list, self.station_profile.station_names))
-        self.cx_profile.create(endp_type="lf_tcp", side_a=self.station_profile.station_names, side_b=self.upstream,
-                               sleep_time=0)
+        # Set up attenuators
+        if self.attenuator is not None:
+            print('Configuring attenuators.......')
+            # self.attenuator.base_profile()
+            print('Finished configuring attenuators!')
+        else:
+            exit('Error: No attenuator detected')
 
-    def start(self):
+    def start_station_traffic(self):
         self.station_profile.admin_up()
         temp_stas = self.station_profile.station_names.copy()
         # temp_stas = sta_list
@@ -200,10 +227,28 @@ class Tele2RateVersusRange(LFCliBase):
             self._fail("Stations failed to get IPs")
             self.exit_fail()
         self.cx_profile.start_cx()
-
+        print('Waiting for these cross endpoints to appear:', self.cx_profile.get_cx_names())
+        self.local_realm.wait_until_cxs_appear(self.cx_profile.get_cx_names())
+        self.wait_for_cx_to_start(self.cx_profile.get_cx_names())
+        time.sleep(1)
+ 
+    def start(self):
+        print('Starting RvR test for {} iterations...'.format(self.num_iterations))
+        self.precleanup()
+        self.create_station() 
+        for i in range(self.num_iterations):
+            for dut in self.duts:
+                for step, ddb in enumerate(range(0, 1000, self.attenuator.ddb_step_size)):
+                    if step != 0:
+                        self.attenuator.increase_attenuation(i)
+                    self.start_station_traffic()
+                    print('[Iteration={}|DUT={}|atten={}] Running traffic on stations for {} seconds..'.format(self.num_iterations, dut, ddb, self.step_length_sec))
+                    time.sleep(self.step_length_sec)
+                    self.stop()
 
     def stop(self):
         # Bring stations down
+        print('Bringing stations down..')
         self.station_profile.admin_down()
         self.cx_profile.stop_cx()
 
@@ -211,6 +256,7 @@ class Tele2RateVersusRange(LFCliBase):
 def main():
     parser = argparse.ArgumentParser(description="Tele2 RvR Script")
     parser.add_argument('-hst', '--host', type=str, help='host name')
+    parser.add_argument('-prt', '--port', type=int, help='host port')
     parser.add_argument('-s', '--ssid', type=str, help='ssid for client')
     parser.add_argument('-pwd', '--passwd', type=str, help='password to connect to ssid')
     parser.add_argument('-sec', '--security', type=str, help='security')
@@ -218,12 +264,13 @@ def main():
     #parser.add_argument()
     args = parser.parse_args()
     num_sta = 1
+    duts = ['L2']
     station_list = LFUtils.port_name_series(prefix="sta",
                                             start_id=0,
                                             end_id=num_sta - 1,
                                             padding_number=10000,
                                             radio=args.radio)
-    obj = Tele2RateVersusRange(lfclient_host= args.host, lfclient_port=8080, ssid=args.ssid , paswd=args.passwd, security=args.security, radio=args.radio, sta_list=station_list)
+    obj = Tele2RateVersusRange(lfclient_host= args.host, lfclient_port=args.port, ssid=args.ssid , paswd=args.passwd, security=args.security, radio=args.radio, sta_list=station_list, duts=duts)
     obj.precleanup()
     obj.build()
     obj.start()
